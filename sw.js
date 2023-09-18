5f1710d64d375b6ffc7615ea2c54bd38445dd964
Daniel Opara <67028678+danielopara@users.noreply.github.com>
2023-09-18 11:10:21 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@ const assets = [
     '/assets/img/paper-plane1.ico',
     '/dist/style.css',
     "/assets/img/menu.png",
-    "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
+    // "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css",
     '/assets/img/paper-plane2.ico',
     "/assets/img/visibility_off.png",
     "/assets/img/visibility.png",
